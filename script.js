36e3611369dcb7550b623fe07f45789cf1e071c8
Colm Doyle <collydoyle_88@hotmail.com>
2023-07-07 12:25:20 +0000

    updated fault loop section and functionality

--- script.js
+++ script.js
@@ -17,7 +17,103 @@ function calc() {
   document.getElementById("cable_length_calc").value = cable_length_result;
 }
 
+// Fault loop script
 
-function fault_loop(breaker_type){ 
-    document.getElementById("demo").innerHTML=breaker_type;
+function fault_loop(){
+var type_b = document.getElementById("type_b").checked;
+var type_c = document.getElementById("type_c").checked;
+var type_d = document.getElementById("type_d").checked;
+var mcb_6 = document.getElementById("size_6").checked;
+var mcb_10 = document.getElementById("size_10").checked;
+var mcb_16 = document.getElementById("size_16").checked;
+var mcb_20 = document.getElementById("size_20").checked;
+var mcb_25 = document.getElementById("size_25").checked;
+var mcb_32 = document.getElementById("size_32").checked;
+var mcb_40 = document.getElementById("size_40").checked;
+var mcb_50 = document.getElementById("size_50").checked;
+var mcb_63 = document.getElementById("size_63").checked;
+var mcb_80 = document.getElementById("size_80").checked;
+var mcb_100 = document.getElementById("size_100").checked;
+var mcb_125 = document.getElementById("size_125").checked;
+
+
+if (type_b && mcb_6) {
+    document.getElementById("demo").innerHTML= "7.67Ω";
+  } else if (type_b && mcb_10)  {
+    document.getElementById("demo").innerHTML= "4.60Ω";
+  } else if (type_b && mcb_16)  {
+    document.getElementById("demo").innerHTML= "2.87Ω";
+  } else if (type_b && mcb_20)  {
+    document.getElementById("demo").innerHTML= "2.30Ω";
+  } else if (type_b && mcb_25)  {
+    document.getElementById("demo").innerHTML= "1.84Ω";
+  } else if (type_b && mcb_32)  {
+    document.getElementById("demo").innerHTML= "1.44Ω";
+  } else if (type_b && mcb_40)  {
+    document.getElementById("demo").innerHTML= "1.15Ω";
+  } else if (type_b && mcb_50)  {
+    document.getElementById("demo").innerHTML= "0.92Ω";
+  } else if (type_b && mcb_63)  {
+    document.getElementById("demo").innerHTML= "0.73Ω";
+  } else if (type_b && mcb_80)  {
+    document.getElementById("demo").innerHTML= "0.57Ω";
+  } else if (type_b && mcb_100)  {
+    document.getElementById("demo").innerHTML= "0.46Ω";
+  } else if (type_b && mcb_125)  {
+    document.getElementById("demo").innerHTML= "0.37Ω";
+  } else if (type_c && mcb_6)  {
+    document.getElementById("demo").innerHTML= "3.83Ω";
+  } else if (type_c && mcb_10)  {
+    document.getElementById("demo").innerHTML= "2.30Ω";
+  } else if (type_c && mcb_16)  {
+    document.getElementById("demo").innerHTML= "1.44Ω";
+  } else if (type_c && mcb_20)  {
+    document.getElementById("demo").innerHTML= "1.15Ω";
+  } else if (type_c && mcb_25)  {
+    document.getElementById("demo").innerHTML= "0.92Ω";
+  } else if (type_c && mcb_32)  {
+    document.getElementById("demo").innerHTML= "0.73Ω";
+  } else if (type_c && mcb_40)  {
+    document.getElementById("demo").innerHTML= "0.57Ω";
+  } else if (type_c && mcb_50)  {
+    document.getElementById("demo").innerHTML= "0.46Ω";
+  } else if (type_c && mcb_63)  {
+    document.getElementById("demo").innerHTML= "0.36Ω";
+  } else if (type_c && mcb_80)  {
+    document.getElementById("demo").innerHTML= "0.29Ω";
+  } else if (type_c && mcb_100)  {
+    document.getElementById("demo").innerHTML= "0.23Ω";
+  } else if (type_c && mcb_125)  {
+    document.getElementById("demo").innerHTML= "0.18Ω";
+  } else if (type_d && mcb_6)  {
+    document.getElementById("demo").innerHTML= "1.92Ω";
+  } else if (type_d && mcb_10)  {
+    document.getElementById("demo").innerHTML= "1.15Ω";
+  } else if (type_d && mcb_16)  {
+    document.getElementById("demo").innerHTML= "0.72Ω";
+  } else if (type_d && mcb_20)  {
+    document.getElementById("demo").innerHTML= "0.57Ω";
+  } else if (type_d && mcb_25)  {
+    document.getElementById("demo").innerHTML= "0.46Ω";
+  } else if (type_d && mcb_32)  {
+    document.getElementById("demo").innerHTML= "0.36Ω";
+  } else if (type_d && mcb_40)  {
+    document.getElementById("demo").innerHTML= "0.29Ω";
+  } else if (type_d && mcb_50)  {
+    document.getElementById("demo").innerHTML= "0.23Ω";
+  } else if (type_d && mcb_63)  {
+    document.getElementById("demo").innerHTML= "0.18Ω";
+  } else if (type_d && mcb_80)  {
+    document.getElementById("demo").innerHTML= "0.14Ω";
+  } else if (type_d && mcb_100)  {
+    document.getElementById("demo").innerHTML= "0.11Ω";
+  } else if (type_d && mcb_125)  {
+    document.getElementById("demo").innerHTML= "0.09Ω";  
+  } else {
+    document.getElementById("demo").innerHTML= "Select Type and Size";
 }
+}
+
+// function fault_loop(breaker_type){ 
+//   document.getElementById("demo").innerHTML=breaker_type;
+// }
